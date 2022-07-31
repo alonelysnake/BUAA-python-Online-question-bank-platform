4a518fb526705aaf4dc70757f3a130dee373c73d
王永瑶“git config --global user.email “19374223@buaa.edu.cn <19374223@buaa.edu.cn>
2022-07-31 21:08:16 +0800

    初始化数据库

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import sys
 
-from ui import WindowController,ChooseQuestion,MyChooseQuestion
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication
+from question.QuestionBank import QuestionBank
+from question.Question import Question
+from function.ExamGeneration import ExamGeneration
+from DatabaseUtil import DB
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mainWindow = QMainWindow()
-    window = MyChooseQuestion.MyChooseQuestion(mainWindow)
-    #window.setupUi(mainWindow)
-    mainWindow.show()
-    #controller= WindowController.WindowController()
-    sys.exit(app.exec_())
+    # app = QApplication(sys.argv)
+    # mainWindow = QMainWindow()
+    # window = MyReviseLoadFile.MyReviseLoadFile(mainWindow)
+    # mainWindow.show()
+    # controller= WindowController.WindowController()
+    # sys.exit(app.exec_())
+    db = DB()
+    db.initial()
