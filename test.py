05ba2855a34a6e13fe438473e7f87ae57400d535
unknown <2462144954@qq.com>
2022-08-04 21:03:22 +0800

    支持pdf,注意看test.py写法

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from ocrpart.ocrclass import Paddleocr
 
-img_path = 'E:/year2(summer)/python_little_semaster/bighw/ocrpart/ppocr_img/ch/'
-img_name = '1.jpg'
-ocr = Paddleocr(img_path, img_name)
-a = ocr.get_result()
+#pdf
+img_dir = 'E:/year2(summer)/python_little_semaster/bighw/ocrpart/ppocr_img/ch/'
+img_name = '1'
+imgtype = 'pdf'
+ocr = Paddleocr(img_dir, img_name, imgtype)
+a = ocr.forward()
 print(a)
-img_name = '2.jpg'
-ocr.change_img(img_path, img_name)
-a = ocr.get_result()
+#img
+imgtype = 'jpg'
+ocr.change_img(img_dir, img_name, imgtype)
+a = ocr.forward()
 print(a)
